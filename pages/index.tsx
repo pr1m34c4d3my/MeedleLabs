4ca0644beb8ebabd8bb3e120ef754d0ec3bce235
Wisnu <wisnubl1995@gmail.com>
2022-05-01 21:57:44 +0700

    Profile Card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '@components/Navbar';
+import Navbar from '@components/Navbar'
+import ProfileCard from '@components/ProfileCard'
 
 const Home: NextPage = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
-      <h1 className='font-black '>Hello World!</h1>
+        <div className="container mx-auto pt-10">
+          <ProfileCard />
+        </div>
+      <h1>Hellow</h1>
     </div>
   )
 }
