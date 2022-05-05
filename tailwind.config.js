e46576be7ffc24945a84ff84205ecd2f053c709d
Wisnu <wisnubl1995@gmail.com>
2022-05-06 02:23:26 +0700

    Update Categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-  mode:'jit',
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      fontFamily:{
-        'sans': ['Poppins' , 'Arial', 'sans-serif']
-      }
+      fontFamily: {
+        sans: ['Poppins', 'Arial', 'sans-serif'],
+      },
+      backgroundImage: {
+        nft: "url('/nfthunt.png')",
+        charities: "url('/charities.png')",
+        jobs: "url('/jobs.png')",
+      },
     },
   },
   plugins: [],
